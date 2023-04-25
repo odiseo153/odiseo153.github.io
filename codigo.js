28195f16e0b3e7d9257f6f4ec84070eb69a7a7fa
odiseo153 <mega09elne@gmail.com>
2023-04-25 12:39:54 -0400

    pagina agregada

--- codigo.js
+++ codigo.js
@@ -1,13 +1,13 @@
 
 
 var caja = document.getElementById('tarjeta')
-
+let url ='https://api.themoviedb.org/3/movie/popular?api_key=64bb1282d5dc2ff52d51cad2f09345c2&language=en-US&page=1'
 
 function getDatos() {
-  let url = 'https://api.themoviedb.org/3/movie/popular?api_key=64bb1282d5dc2ff52d51cad2f09345c2&language=en-US&page=1';
+  let url = '/datos.json';
   fetch(url)
     .then(dato => dato.json())
-    .then(data => mostrarDatos(data.results))
+    .then(data => mostrarDatos(data))
     .catch(err => 
       {
         inabilitar('Algo salio mal revise su conexcion')
@@ -26,22 +26,22 @@ const mostrarDatos = (dato) => {
   
   dato.forEach(e => {
 
+    
     caja.innerHTML += `
   <br> 
     <div class="card" style="width: 18rem;" id="pelis">
-    <img class="card-img-top" src="${img+e.poster_path}" alt="Card image cap">
-    <div class="card-body">
+    <img class="card-img-top" src="${e.imagenDetras}" alt="Card image cap">
+    <div class="card-body" id="texto">
     <p class="card-text" id="id">${e.id}</p>
-    <h5 class="card-title">Titulo: ${e.original_title}</h5> 
-    <p class="card-text">Description: ${e.overview}</p>
+    <h5 class="card-title">Titulo: ${e.titulo}</h5> 
+    <p class="card-text">Description: ${e.descripcion}</p>
 
-    <p class="card-text">Año: ${e.release_date }</p>
+    <p class="card-text">Año: ${e.fecha}</p>
 
     </div>
   </div>
   <br>
 `
-
       })
 
 }
